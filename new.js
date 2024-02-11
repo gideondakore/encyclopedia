7f487e68a881bf312dca7890a4636a9d493e020e
gideondakore <armstrongspycon27@gmail.com>
2024-02-11 18:40:24 +0000

    using the local storage to save user encyclopedia entry

--- new.js
+++ new.js
@@ -2,11 +2,40 @@ import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 const markdownInput = document.getElementById("markdown");
 const outputDiv = document.getElementById("output");
+const title = document.getElementById("title");
+const btnSave = document.querySelector(".btn-save");
+const author = document.getElementById("author");
 
 markdownInput.addEventListener("input", function (e) {
+  if (title.value === "" || author.value === "") {
+    alert("Give a title and author of your entry before the content");
+    title.focus();
+    e.target.value = "";
+    return;
+  }
+  e.preventDefault();
+  console.log(title.value);
   const markdownText = e.target.value;
-
   const htmlText = marked(markdownText);
-
+  console.log(htmlText);
   outputDiv.innerHTML = htmlText;
+});
+
+let entryObj = {};
+btnSave.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!markdownInput.value || !author.value || !title.value) {
+    alert("Give a title, author and content of your entry before saving");
+    author.focus();
+    title.value = "";
+    markdownInput.value = "";
+    return;
+  }
+
+  entryObj.author = author.value;
+  entryObj.title = title.value;
+  entryObj.body = marked.parse(markdownInput.value);
+  const jsonData = JSON.stringify(entryObj);
+  localStorage.setItem("newEntry", jsonData);
 });
